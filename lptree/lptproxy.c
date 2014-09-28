d437db527fffe610d7b43ef853852a4d1e4619f5
deglingo <ahmidmefes@gmail.com>
2014-09-28 16:35:22 +0200

    added the LptProxyMessage enum

--- lptree/lptproxy.c
+++ lptree/lptproxy.c
@@ -10,6 +10,25 @@
 
 
 
+/* LptProxyMessage:
+ */
+typedef enum _LptProxyMessage
+  {
+    LPT_PROXY_MESSAGE_REQUEST_CONNECT = 0,
+    LPT_PROXY_MESSAGE_CONFIRM_CONNECT,
+    LPT_PROXY_MESSAGE_COUNT,
+  }
+  LptProxyMessage;
+
+static LInt *lpt_proxy_message_cache[LPT_PROXY_MESSAGE_COUNT] = { NULL, };
+
+static LInt *lpt_proxy_message_ref ( LptProxyMessage msg )
+{
+  return l_object_ref(lpt_proxy_message_cache[msg]);
+}
+
+
+
 static void _dispose ( LObject *object );
 
 
@@ -18,7 +37,10 @@ static void _dispose ( LObject *object );
  */
 static void lpt_proxy_class_init ( LObjectClass *cls )
 {
+  guint m;
   cls->dispose = _dispose;
+  for (m = 0; m < LPT_PROXY_MESSAGE_COUNT; m++)
+    lpt_proxy_message_cache[m] = l_int_new(m);
 }
 
 
@@ -49,38 +71,61 @@ static void _dispose ( LObject *object )
 
 
 
+/* _handle_request_connect:
+ */
+static void _handle_request_connect ( LptProxy *proxy,
+                                      LInt *clid,
+                                      LObject *msg )
+{
+  LTuple *answer = l_tuple_newl_give(2,
+                                     lpt_proxy_message_ref(LPT_PROXY_MESSAGE_CONFIRM_CONNECT),
+                                     l_object_ref(clid),
+                                     NULL);
+  proxy->handler(proxy, L_INT_VALUE(clid), L_OBJECT(answer), proxy->handler_data);
+  l_object_unref(answer);
+}
+
+
+
+/* _handle_confirm_connect:
+ */
+static void _handle_confirm_connect ( LptProxy *proxy,
+                                      LInt *clid,
+                                      LObject *msg )
+{
+  LptNSpec *ns = lpt_nspec_dir_new("DIR");
+  lpt_tree_create_node(proxy->tree, "/dest-share1", ns);
+  l_object_unref(ns);
+}
+
+
+
 /* lpt_proxy_handle_message:
  */
 void lpt_proxy_handle_message ( LptProxy *proxy,
                                 LObject *msg )
 {
   LObject *key;
-  guint clid;
+  LInt *clid;
   ASSERT(L_IS_TUPLE(msg));
   ASSERT(L_TUPLE_SIZE(msg) >= 2);
+  /* get key */
   key = L_TUPLE_ITEM(msg, 0);
   ASSERT(L_IS_INT(key));
-  ASSERT(L_IS_INT(L_TUPLE_ITEM(msg, 1)));
-  clid = L_INT_VALUE(L_TUPLE_ITEM(msg, 1));
-  switch (L_INT_VALUE(key))
-    {
-    case 1:
-      {
-        LTuple *answer = l_tuple_newl_give(2, l_int_new(2), l_int_new(clid), NULL);
-        proxy->handler(proxy, clid, L_OBJECT(answer), proxy->handler_data);
-        l_object_unref(answer);
-      }
-      break;
-    case 2:
-      {
-        LptNSpec *ns = lpt_nspec_dir_new("DIR");
-        lpt_tree_create_node(proxy->tree, "/dest-share1", ns);
-        l_object_unref(ns);
-      }
-      break;
-    default:
-      CL_DEBUG("[TODO] message key: %d", L_INT_VALUE(key));
-    }
+  /* get clid */
+  clid = L_INT(L_TUPLE_ITEM(msg, 1));
+  ASSERT(L_IS_INT(clid));
+  /* handle key */
+  switch (L_INT_VALUE(key)) {
+  case LPT_PROXY_MESSAGE_REQUEST_CONNECT:
+    _handle_request_connect(proxy, clid, msg);
+    break;
+  case LPT_PROXY_MESSAGE_CONFIRM_CONNECT:
+    _handle_confirm_connect(proxy, clid, msg);
+    break;
+  default:
+    CL_DEBUG("[TODO] msg key: %d", L_INT_VALUE(key));
+  }
 }
 
 
@@ -114,7 +159,10 @@ void lpt_proxy_connect_share ( LptProxy *proxy,
                                gint flags )
 {
   LTuple *msg;
-  msg = l_tuple_newl_give(2, l_int_new(1), l_int_new(clid), NULL);
+  msg = l_tuple_newl_give(2,
+                          lpt_proxy_message_ref(LPT_PROXY_MESSAGE_REQUEST_CONNECT),
+                          l_int_new(clid),
+                          NULL);
   proxy->handler(proxy, clid, L_OBJECT(msg), proxy->handler_data);
   l_object_unref(msg);
 }
