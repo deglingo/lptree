e0fd4628919bd4f2cd898b111bdc3e91703c2453
deglingo <ahmidmefes@gmail.com>
2014-09-30 16:19:43 +0200

    implemented LptNSpecInt.get/from_state()

--- lptree/lptnspec.c
+++ lptree/lptnspec.c
@@ -1,12 +1,16 @@
 /* lptnspec.c -
  */
 
+#include "lptree/private.h"
 #include "lptree/lptnspec.h"
 #include "lptree/lptnspec.inl"
 
 
 
 static void _dispose ( LObject *object );
+static LObject *_get_state ( LObject *object );
+static LObject *_from_state ( LObjectClass *cls,
+                              LObject *state );
 
 static guint nsid_counter = 1;
 
@@ -17,6 +21,8 @@ static guint nsid_counter = 1;
 static void lpt_nspec_class_init ( LObjectClass *cls )
 {
   cls->dispose = _dispose;
+  cls->get_state = _get_state;
+  cls->from_state = _from_state;
 }
 
 
@@ -37,6 +43,32 @@ static void _dispose ( LObject *object )
   g_free(LPT_NSPEC(object)->name);
   LPT_NSPEC(object)->name = NULL;
   ((LObjectClass *) parent_class)->dispose(object);
+}
+
+
+
+/* _get_state:
+ */
+static LObject *_get_state ( LObject *object )
+{
+  return L_OBJECT(l_tuple_newl_give(1,
+                                    l_string_new(LPT_NSPEC(object)->name),
+                                    NULL));
+}
+
+
+
+/* _from_state:
+ */
+static LObject *_from_state ( LObjectClass *cls,
+                              LObject *state )
+{
+  LObject *nspec;
+  ASSERT(L_IS_TUPLE(state));
+  ASSERT(L_TUPLE_SIZE(state) == 1);
+  nspec = l_object_new(cls, NULL);
+  LPT_NSPEC(nspec)->name = g_strdup(L_STRING(L_TUPLE_ITEM(state, 0))->str);
+  return nspec;
 }
 
 
--- lptree/lptnspecint.c
+++ lptree/lptnspecint.c
@@ -1,8 +1,25 @@
 /* lptnspecint.c -
  */
 
+#include "lptree/private.h"
 #include "lptree/lptnspecint.h"
 #include "lptree/lptnspecint.inl"
+
+
+
+static LObject *_get_state ( LObject *object );
+static LObject *_from_state ( LObjectClass *cls,
+                              LObject *state );
+
+
+
+/* lpt_nspec_int_class_init:
+ */
+static void lpt_nspec_int_class_init ( LObjectClass *cls )
+{
+  cls->get_state = _get_state;
+  cls->from_state = _from_state;
+}
 
 
 
@@ -21,4 +38,36 @@ LptNSpec *lpt_nspec_int_new ( const gchar *name,
   nspec->maxi = maxi;
   nspec->defo = defo;
   return LPT_NSPEC(nspec);
+}
+
+
+
+/* _get_state:
+ */
+static LObject *_get_state ( LObject *object )
+{
+  LObject *parent = ((LObjectClass *) parent_class)->get_state(object);
+  LTuple *t = l_tuple_newl_give(4,
+                                parent,
+                                l_int_new(LPT_NSPEC_INT(object)->mini),
+                                l_int_new(LPT_NSPEC_INT(object)->maxi),
+                                l_int_new(LPT_NSPEC_INT(object)->defo),
+                                NULL);
+  return L_OBJECT(t);
+}
+
+
+
+/* _from_state:
+ */
+static LObject *_from_state ( LObjectClass *cls,
+                              LObject *state )
+{
+  ASSERT(L_IS_TUPLE(state));
+  ASSERT(L_TUPLE_SIZE(state) == 4);
+  LObject *nspec = ((LObjectClass *) parent_class)->from_state(cls, L_TUPLE_ITEM(state, 0));
+  LPT_NSPEC_INT(nspec)->mini = L_INT_VALUE(L_TUPLE_ITEM(state, 1));
+  LPT_NSPEC_INT(nspec)->maxi = L_INT_VALUE(L_TUPLE_ITEM(state, 2));
+  LPT_NSPEC_INT(nspec)->defo = L_INT_VALUE(L_TUPLE_ITEM(state, 3));
+  return nspec;
 }
